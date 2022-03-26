4a2ab4f73029e23f59f882d291eb7f9ef3b43b8b
sadamshah1713@gmail.com <sadamshah1713@gmail.com>
2022-03-26 15:20:52 +0500

    da wala pera

--- TestGitHub/TestGitHub/Form1.cs
+++ TestGitHub/TestGitHub/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,18 @@ namespace TestGitHub
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //Vhecking
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\saddam\Source\Repos\MyProject\TestGitHub\TestGitHub\Database1.mdf;Integrated Security=True");
+            try
+            {
+                if(con!= null)
+                {
+                    MessageBox.Show("Connection Established");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
